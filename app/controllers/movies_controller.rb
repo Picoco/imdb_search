c2f14c6692977f1f77ed46507eca2f6c6a1f808e
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-07-26 16:45:07 +0200

    ok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+
+    #  what do i have to put here instead of this search
+    if params[:query].present?
+
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.syllabus @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
